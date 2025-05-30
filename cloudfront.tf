eaa822cd6bffa57933179f049d1c608f9788aff0
1ahmedharris <24aitc@gmail.com>
2025-05-30 17:48:08 -0400

    v1.1

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,10 +1,16 @@
 # cloudfront.tf
 
-# Define local values for reusable strings within this configuration
 locals {
-  s3_origin_id = "s3Origin" 
+  s3_origin_id = "aitc-s3-origin" 
+  # Collect all the ETags from the S3 objects managed by Terraform.
+  # s3_objects_etags_list = [for obj in aws_s3_object.website_files : obj.etag]
+  # Create a unique hash from the sorted list of ETags.
+  # cloudfront_invalidation_hash = sha1(join("", sort(local.s3_objects_etags_list)))
 }
 
+data "aws_cloudfront_cache_policy" "caching_optimized" {
+  name = "Managed-CachingOptimized"
+}
 
 resource "aws_cloudfront_origin_access_control" "cloudfront_oac" {
   name                              = "cnd-oac"
@@ -12,12 +18,6 @@ resource "aws_cloudfront_origin_access_control" "cloudfront_oac" {
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
-
-# Data Source retrieves ID of AWS managed CachingOptimized cache policy
-data "aws_cloudfront_cache_policy" "caching_optimized" {
-  name = "Managed-CachingOptimized"
-}
-
 
 resource "aws_cloudfront_distribution" "cloudfront_distro" {
   enabled             = true
@@ -28,15 +28,15 @@ resource "aws_cloudfront_distribution" "cloudfront_distro" {
   aliases             = var.cloudfront_aliases
   web_acl_id          = var.cloudfront_web_acl_arn 
   wait_for_deployment = true
-  # --- Origin Configuration ---
+  http_version        = "http2"
+
   origin {
     domain_name              = aws_s3_bucket.bucket_config.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_oac.id
     origin_id                = local.s3_origin_id 
-    connection_attempts      = 2
+    connection_attempts      = 3
     connection_timeout       = 7
   }
-
 
   default_cache_behavior {
     cache_policy_id            = data.aws_cloudfront_cache_policy.caching_optimized.id
@@ -48,12 +48,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distro" {
     max_ttl                    = 0
     min_ttl                    = 0
     viewer_protocol_policy     = "https-only"
-
-    grpc_config {
-      enabled = false
-    }
   }
-
 
   restrictions {
     geo_restriction {
@@ -62,7 +57,6 @@ resource "aws_cloudfront_distribution" "cloudfront_distro" {
     }
   }
 
-
   viewer_certificate {
     acm_certificate_arn        = var.acm_certificate_arn
     minimum_protocol_version   = "TLSv1.2_2021"
@@ -70,3 +64,15 @@ resource "aws_cloudfront_distribution" "cloudfront_distro" {
   }
 
 }
+
+
+# resource "aws_cloudfront_invalidation" "s3_content_invalidation" {
+#  distribution_id = aws_cloudfront_distribution.cloudfront_distro.id
+#  paths = ["/*"]
+#  # caller_reference must change for CloudFront to create a new invalidation.
+#  caller_reference = local.cloudfront_invalidation_hash
+  # Invalidation runs AFTER the S3 objects have been updated.
+#  depends_on = [
+#    aws_s3_object.website_files
+#  ]
+#}
