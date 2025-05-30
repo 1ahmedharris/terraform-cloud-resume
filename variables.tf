d0fedbddf3f3eff7fc8e36b8bc819149f6520ab0
1ahmedharris <24aitc@gmail.com>
2025-05-30 17:48:59 -0400

    v1.1

--- variables.tf
+++ variables.tf
@@ -1,38 +1,37 @@
-# variables.tf
+
 
 variable "website_content_path" {
-  description = "The local file system path to the root of your website content."
+  description = "Local file system path to the root directory of website content."
   type        = string
 }
 
 
 variable "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate for the CloudFront distribution's custom domain."
+  description = "The ARN of ACM certificate for CloudFront distribution's custom domain."
+  type        = string
+}
+
+
+variable "cloudfront_web_acl_arn" {
+  description = "The ARN of AWS WAF Web ACL to associate with CloudFront distribution."
   type        = string
 }
 
 
 variable "cloudfront_aliases" {
-  description = "List of domain names for the CloudFront distribution."
+  description = "List of all domain name aliases used by the CloudFront distribution."
   type        = list(string)
 }
 
 
-variable "cloudfront_web_acl_arn" {
-  description = "The ARN of an AWS WAF Web ACL to associate with the CloudFront distribution. Set to null if not used."
-  type        = string
-}
-
-
 variable "cloudfront_restriction_locations" {
-  description = "A list of country codes (ISO 3166-1 alpha-2) for geo-restriction."
+  description = "List of country codes for geo-restriction."
   type        = list(string)
 }
 
 
 variable "cloudfront_restriction_type" {
-  description = "The type of geo-restriction: 'none', 'whitelist', or 'blacklist'."
+  description = "Type of geo-restriction."
   type        = string
 }
-
 
