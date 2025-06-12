1b808b37049d383c09b2112574dc98b4a0323419
1ahmedharris <24aitc@gmail.com>
2025-06-12 13:16:49 -0400

    v1

---
+++ dynamodb.tf
@@ -0,0 +1,53 @@
+resource "aws_dynamodb_table" "visitor_count_table" {
+  name         = "visitor-count-table"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  hash_key = "id"
+
+  point_in_time_recovery {
+    enabled = false
+  }
+
+  ttl {
+    attribute_name = ""
+    enabled        = false
+  }
+}
+
+# Manages the "views" attribute
+resource "aws_dynamodb_table_item" "visitor_count_initial_item" {
+  table_name = aws_dynamodb_table.visitor_count_table.name
+  hash_key   = aws_dynamodb_table.visitor_count_table.hash_key
+
+  item = <<ITEM
+    {
+        "id": {"S": "${var.counter}"},    
+        "views": {"N": "${var.views_count}"} 
+    }
+ITEM
+
+  depends_on = [
+    aws_dynamodb_table.visitor_count_table
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      item,
+    ]
+  }
+}
+
+output "dynamodb_table_name_output" {
+  value       = aws_dynamodb_table.visitor_count_table.name
+  description = "The name of the DynamoDB visitor count table."
+}
+
+output "dynamodb_table_arn_output" {
+  value       = aws_dynamodb_table.visitor_count_table.arn
+  description = "The ARN of the DynamoDB visitor count table."
+}
