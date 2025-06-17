242287660a41ae11c27bb2099d3f10805e3fe565
1ahmedharris <24aitc@gmail.com>
2025-06-16 23:33:17 -0400

    v1

--- route53.tf
+++ route53.tf
@@ -1,17 +1,16 @@
 #Public hosted zone ahmedharrisresume.com
 resource "aws_route53_zone" "www_resume_zone" {
-  name    = "ahmedharrisresume.com"
-  zone_id =  "Z01018062M98VOF9SUSIM"
+  name    = var.main_resume_domain_name
 }
 
 # Alias record for root domain ahmedharrisresume.com pointing to CloudFront
 resource "aws_route53_record" "www_resume_root_alias" {
   zone_id                = aws_route53_zone.www_resume_zone.zone_id
-  name                   = "ahmedharrisresume.com"
+  name                   = var.main_resume_domain_name
   type                   = "A"
   alias {
-    name                   = [aws_cloudfront_distribution.cloudfront_cdn.domain_name]
-    zone_id                = [aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id]
+    name                   = aws_cloudfront_distribution.cloudfront_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -19,40 +18,40 @@ resource "aws_route53_record" "www_resume_root_alias" {
 # Alias record for subdomain www.ahmedharrisresume.com pointing to CloudFront
 resource "aws_route53_record" "www_resume_subdomain_alias" {
   zone_id                = aws_route53_zone.www_resume_zone.zone_id
-  name                   = "www.ahmedharrisresume.com" 
+  name                   = var.main_resume_subdomain
   type                   = "A"
   alias {
-    name                   = [aws_cloudfront_distribution.cloudfront_cdn.domain_name]
-    zone_id                = [aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id]
+    name                   = aws_cloudfront_distribution.cloudfront_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
 #Public hosted zone ahmedharrisdevops.com
 resource "aws_route53_zone" "www_devops_zone" {
-  name = "ahmedharrisdevops.com"
+  name = var.devops_resume_domain_name
 }
 
 # Alias record for root domain ahmedharrisdevops.com pointing to CloudFront
 resource "aws_route53_record" "www_devops_root_alias" {
   zone_id                = aws_route53_zone.www_devops_zone.zone_id
-  name                   = "ahmedharrisdevops.com"
+  name                   = var.devops_resume_domain_name
   type                   = "A"
   alias {
-    name                   = [aws_cloudfront_distribution.cloudfront_cdn.domain_name]
-    zone_id                = [aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id]
+    name                   = aws_cloudfront_distribution.cloudfront_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-# Alias record for subdomain www.ahmedharrisresume.com pointing to CloudFront
+# Alias record for subdomain www.ahmedharrisdevops.com pointing to CloudFront
 resource "aws_route53_record" "www_devops_subdomain_alias" {
   zone_id                = aws_route53_zone.www_devops_zone.zone_id
-  name                   = "www.ahmedharrisdevops.com" # This is the www subdomain name
+  name                   = var.devops_resume_subdomain
   type                   = "A"
   alias {
-    name                   = [aws_cloudfront_distribution.cloudfront_cdn.domain_name]
-    zone_id                = [aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id]
+    name                   = aws_cloudfront_distribution.cloudfront_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront_cdn.hosted_zone_id
     evaluate_target_health = false
   }
 }
