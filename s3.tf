3c438cf41ba9c62256592ce1fdf3dc61fcd95ab5
1ahmedharris <24aitc@gmail.com>
2025-06-17 15:24:57 -0400

    fix: refactored attribute values

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_canonical_user_id" "current" {}
 
-
 resource "aws_s3_bucket" "bucket_config" {
-  bucket = "aitc-s3"
+  bucket = var.s3_bucket 
 }
-
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_config_sse" {
   bucket = aws_s3_bucket.bucket_config.id
-
+  
   rule {
     bucket_key_enabled = true 
     apply_server_side_encryption_by_default {
@@ -16,7 +16,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_config_sse
     }
   }
 }
-
 
 resource "aws_s3_bucket_versioning" "bucket_config_versioning" {
   bucket = aws_s3_bucket.bucket_config.id
@@ -26,7 +25,6 @@ resource "aws_s3_bucket_versioning" "bucket_config_versioning" {
     mfa_delete = "Disabled" 
   }
 }
-
 
 resource "aws_s3_bucket_policy" "bucket_config_policy" {
   bucket = aws_s3_bucket.bucket_config.id
@@ -45,7 +43,8 @@ resource "aws_s3_bucket_policy" "bucket_config_policy" {
           Resource  = "${aws_s3_bucket.bucket_config.arn}/*"
           Condition = {
             StringEquals = {
-              "AWS:SourceArn" = "arn:aws:cloudfront::941377150270:distribution/E11U5R2YIBF4OY" # Hardcoded CloudFront ARN
+              "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cloudfront_cdn.id}"
+
             }
           }
         },
@@ -54,18 +53,15 @@ resource "aws_s3_bucket_policy" "bucket_config_policy" {
   )
 }
 
-
-# Define local filepath values to iterate through
-# Discover ALL files recursively within the specified website_content_path
+# Defines site local filepath values 
 locals {
   files = fileset(var.website_content_path, "**/*")
-  # Create map where keys are the S3 object keys
   object_map = {
     for file in local.files :
     file => {
-      source_path  = "${var.website_content_path}/${file}" # Full local path to the file
+      source_path  = "${var.website_content_path}/${file}" 
       content_type = lookup(
-        { # Map common file extensions to their MIME types
+        {
           "html" : "text/html",
           "css" : "text/css",
           "png" : "image/png",
@@ -73,21 +69,19 @@ locals {
           "jpeg" : "image/jpeg", 
           "ico" : "image/x-icon", 
         },
-        # Extract the file extension from the file name
-        split(".", file)[length(split(".", file)) - 1],
+        split(".", file)[length(split(".", file)) - 1], # Extract the file extension from the file name
         "application/octet-stream" # Default if extension not found in map
       )
     }
   }
 }
 
-
-# Create an aws_s3_object resource for each file found in the object_map.
+# Creates an s3 object resource for each file found in the locals object_map.
 resource "aws_s3_object" "website_files" {
   for_each = local.object_map 
   bucket = aws_s3_bucket.bucket_config.id 
   key    = each.key                       
   source = each.value.source_path        
   content_type = each.value.content_type
-  etag = filemd5(each.value.source_path)  # Change detection and CloudFront
+  etag = filemd5(each.value.source_path)  # ile change detection 
 }
