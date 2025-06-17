1140007a1c8efc3f86b0c24d4a9d11a3c5b87730
1ahmedharris <24aitc@gmail.com>
2025-06-17 16:20:21 -0400

    fix: refactored attribute values

--- cloudfront.tf
+++ cloudfront.tf
@@ -7,7 +7,7 @@ data "aws_cloudfront_cache_policy" "caching_optimized" {
 }
 
 resource "aws_cloudfront_origin_access_control" "cloudfront_oac" {
-  name                              = "cnd-oac"
+  name                              = "resume-cdn-oac"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
