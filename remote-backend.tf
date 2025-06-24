bf65ca05c37163af6551b9d2ae7339406a534cad
1ahmedharris <24aitc@gmail.com>
2025-06-23 23:56:38 -0400

    feat: added s3 remote backend/state locking

---
+++ remote-backend.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+  bucket         = "resume-remote-backend"  
+  key            = "terraform.tfstate"                       
+  region         = "us-east-1"                       
+  encrypt        = true                                     
+  dynamodb_table = "resume-state-lock-table" 
+  }
+}
+
+resource "aws_s3_bucket" "resume_remote_backend" {
+  bucket = "resume-remote-backend"
+}
+
+resource "aws_dynamodb_table" "resume_state_lock_table" {
+  name         = "resume-state-lock-table" 
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID" 
+
+  attribute {
+    name = "LockID"
+    type = "S" 
+  }
+}
