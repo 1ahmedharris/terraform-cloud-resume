25265b2e1eec501d36f59f31cad67e79820183a6
1ahmedharris <24aitc@gmail.com>
2025-07-12 04:11:18 -0400

    feat: added use_lockfile

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,11 +1,22 @@
 terraform {
   backend "s3" {
-  bucket         = "resume-remote-backend"  
-  key            = "terraform.tfstate"                       
-  region         = "us-east-1"                       
-  encrypt        = true                                     
-  dynamodb_table = "resume-state-lock-table" 
+    bucket         = "resume-remote-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true 
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "resume_remote_backend" {
