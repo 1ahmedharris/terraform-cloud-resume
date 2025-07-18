ec0aab9c91c3a420591ec3d61ad85cfc93b14491
Ahmed Harris <24aitc@gmail.com>
2025-07-18 11:58:45 -0400

    Update github-actions-iam.tf

--- github-actions-iam.tf
+++ github-actions-iam.tf
@@ -46,7 +46,8 @@ resource "aws_iam_policy" "github_actions_resume_policy" {
           "iam:ListRolePolicies",
           "iam:ListAttachedRolePolicies",
           "iam:GetPolicyVersion",
-          "iam:ListPolicyVersions"
+          "iam:ListPolicyVersions",
+          "iam:DeletePolicyVersion"
         ],
         Resource = "*"
       },
