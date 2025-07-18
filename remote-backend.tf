a3ed7075811c38d8526d4054460bfbc16151bb83
Ahmed Harris <24aitc@gmail.com>
2025-07-17 23:55:15 -0400

    feat: using s3 native state locking

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,24 +1,11 @@
 terraform {
+  required_version = ">= 1.11.0"
+
   backend "s3" {
-    bucket         = "resume-remote-backend"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true 
-  }
-}
-
-resource "aws_s3_bucket" "resume_remote_backend" {
-  bucket = "resume-remote-backend"
-}
-
-resource "aws_dynamodb_table" "resume_state_lock_table" {
-  name         = "resume-state-lock-table" 
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID" 
-
-  attribute {
-    name = "LockID"
-    type = "S" 
+    bucket       = "resume-remote-backend"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
